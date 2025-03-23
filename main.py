9b973ce9649ff88ff77bdaeeb3e8674f238104dc
coolxcody <konraddabrowski03@gmail.com>
2025-03-24 00:28:21 +0100

    Finish backend implementation

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import google.generativeai as genai
 import pandas as pd
 import os
+import requests
 from dotenv import load_dotenv
 from prompts import *
+import re
 
 """
 # Dostosowanie bazy do projektu
@@ -12,31 +14,45 @@ df = df.drop(columns=["popularity"])
 df = df.dropna()
 df['avg_weight'] = (df['min_weight'] + df['max_weight']) / 2
 df = df[df['avg_weight'] != 0]
+df = df.drop(columns=['group', 'min_height', 'max_height', 'min_weight', 'max_weight', 'min_expectancy', 'max_expectancy'])
+df = df.loc[:, ~df.columns.str.contains('category')]
 df.to_csv('dog_breeds.csv', index=False)
 """
 
-# Konfiguracja klucza API
+# Wczytanie bazy danych
+dogs = pd.read_csv('dog_breeds.csv')
+
+# Konfiguracja kluczy API
 load_dotenv()
 google_api_key = os.getenv('GOOGLE_API_KEY')
+dog_api_key = os.getenv('DOG_API_KEY')
 
 # Konfiguracja Gemini
 genai.configure(api_key=google_api_key)
 model = genai.GenerativeModel('gemini-1.5-flash')
-
-dogs = pd.read_csv('dog_breeds.csv')
 
 def interpret_input(user_input, prompt_temp):
     """
     Uniwersalny interpreter zapytań użytkownika
     """
     prompt = prompt_temp.format(user_input=user_input)
+
     try:
         response = model.generate_content(contents=prompt)
-        print(response.text)
-        return response.text.strip()
+        result = response.text.strip()
+
+        if result == "0":
+            print("Twoja odpowiedź była niejasna.")
+            user_input = input("Odpowiedz bardziej szczegółowo: ")
+            prompt = prompt_temp.format(user_input=user_input)
+            response = model.generate_content(contents=prompt)
+            result = response.text.strip()
+        return result
+
     except Exception as e:
         print(f"Błąd podczas interpretacji zapytania: {e}")
         return "0"
+
 
 def filter_weight(user_input, data):
     """
@@ -54,65 +70,155 @@ def filter_weight(user_input, data):
 
     return filtered if not filtered.empty else None
 
+
 def filter_grooming(user_input, data):
     """
     Filtracja według częstotliwości pielęgnacji
     """
-    options = interpret_input(user_input, GROOMING_PROMPT)
-    filtered = pd.DataFrame()
-
-    if "2" in options:
-        filtered = data[data['grooming_frequency_value'] <= 0.2]
-    elif "4" in options:
-        filtered = data[data['grooming_frequency_value'] <= 0.4]
-    elif "6" in options:
-        filtered = data[data['grooming_frequency_value'] <= 0.6]
-    elif "8" in options:
-        filtered = data[data['grooming_frequency_value'] <= 0.8]
-    elif "10" in options:
-        filtered = data[data['grooming_frequency_value'] <= 1.0]
+    options = float(re.sub(r"[^\d.]","",interpret_input(user_input, GROOMING_PROMPT)))
+    filtered = data[data['grooming_frequency_value'] <= options]
 
     return filtered if not filtered.empty else None
+
 
 def filter_shedding(user_input, data):
     """
     Filtracja według linienia
     """
-    options = interpret_input(user_input, SHEDDING_PROMPT)
-    filtered = pd.DataFrame()
-
-    if "2" in options:
-        filtered = data[data['shedding_value'] <= 0.2]
-    elif "4" in options:
-        filtered = data[data['shedding_value'] <= 0.4]
-    elif "6" in options:
-        filtered = data[data['shedding_value'] <= 0.6]
-    elif "8" in options:
-        filtered = data[data['shedding_value'] <= 0.8]
-    elif "10" in options:
-        filtered = data[data['shedding_value'] <= 1.0]
+    options = float(re.sub(r"[^\d.]","",interpret_input(user_input, SHEDDING_PROMPT)))
+    filtered = data[data['shedding_value'] <= options]
 
     return filtered if not filtered.empty else None
 
+
+def filter_energy(user_input, data):
+    """
+    Filtracja według zapotrzebowania energetycznego
+    """
+    options = float(re.sub(r"[^\d.]","",interpret_input(user_input, ENERGY_PROMPT)))
+    filtered = data[data['energy_level_value'] <= options]
+
+    return filtered if not filtered.empty else None
+
+def filter_trainability(user_input, data):
+    """
+    Filtracja według podatności na tresurę
+    """
+    options = float(re.sub(r"[^\d.]","",interpret_input(user_input, TRAINABILITY_PROMPT)))
+    filtered = data[data['trainability_value'] >= options]
+
+    return filtered if not filtered.empty else None
+
+
+def filter_demeanor(user_input, data):
+    """
+    Filtracja według postawy w stosunku do obcych
+    """
+    options = float(re.sub(r"[^\d.]","",interpret_input(user_input, DEMEANOR_PROMPT)))
+    filtered = data[data['demeanor_value'] >= options]
+
+    return filtered if not filtered.empty else None
+
+
+def filter_temperament(user_input, data):
+    """
+    Filtracja według temperamentu
+    """
+    prompt_data = data[['breeds', 'temperament']].to_json(orient='records')
+    prompt = TEMPERAMENT_PROMPT.format(user_input=user_input, prompt_data=prompt_data)
+
+    try:
+        response = model.generate_content(contents=prompt)
+        response = response.text.strip()
+        filtered = data[data['breeds'] == response]
+        return filtered if not filtered.empty else None
+
+    except Exception as e:
+        print(f"Błąd podczas interpretacji zapytania: {e}")
+        return None
+
+
+def translate_description(text):
+    """
+    Tłumaczenie opisu
+    """
+    prompt = TRANSLATE_PROMPT.format(text=text)
+
+    try:
+        response = model.generate_content(contents=prompt)
+        translated_text = response.text.strip()
+        return translated_text
+
+    except Exception as e:
+        print(f"Błąd podczas tłumaczenia opisu: {e}")
+        return description
+
+
+def get_dog_image(name):
+    """
+    Pobranie zdjęć po API
+    """
+    search_url = f"https://api.thedogapi.com/v1/breeds/search?q={name}"
+    headers = {"x-api-key": dog_api_key}
+
+    response = requests.get(search_url, headers=headers)
+
+    if response.status_code == 200 and response.json():
+        breed_id = response.json()[0]["id"]
+
+        image_url = f"https://api.thedogapi.com/v1/images/search?breed_ids={breed_id}&limit=1"
+        image_response = requests.get(image_url, headers=headers)
+
+        if image_response.status_code == 200 and image_response.json():
+            return image_response.json()[0]["url"]
+        else:
+            return None
+    else:
+        return None
+
+
 def process_filters(filter_list, dog_list):
+    """
+    Obsługa filtracji
+    """
     for question, filter_func in filter_list:
         answer = input(question)
-        dog_list = filter_func(answer, dog_list)
+        new_list = filter_func(answer, dog_list)
 
-        if dog_list is None:
-            return None
+        if new_list is None:
+            return dog_list
 
-        print("\nFiltracja zakończona, wyniki:")
-        print(dog_list)
+        dog_list = new_list
+
+    if len(dog_list) > 1:
+        temperament_answer = input("Jakie cechy charakteru psa są dla Ciebie najważniejsze? ")
+        dog_list = filter_temperament(temperament_answer, dog_list)
 
     return dog_list
 
+
 filters = [
-    ("Jak duży powinien być Twój idealny pies? ", filter_weight),
+    ("Jaki rozmiar psa najlepiej by Ci odpowiadał? ", filter_weight),
     ("Jak często możesz zajmować się pielęgnacją swojego zwierzaka? ", filter_grooming),
-    ("Jak często może linieć Twój idealny pies? ", filter_shedding)]
+    ("Jak często może linieć Twój idealny pies? ", filter_shedding),
+    ("Jak aktywny powinien być Twój pies? ", filter_energy),
+    ("Jakie są Twoje oczekiwania wobec podatności psa na tresurę? ", filter_trainability),
+    ("Jaki sposób zachowania psa wobec obcych najlepiej by Ci odpowiadał? " , filter_demeanor)]
 
 filtered_dogs = process_filters(filters, dogs)
 
-if filtered_dogs is None:
+if filtered_dogs is not None and not filtered_dogs.empty:
+    breed_name = filtered_dogs.iloc[0]["breeds"]
+    description = filtered_dogs.iloc[0]["description"]
+    translated_description = translate_description(description)
+
+    print(f"\nNajlepiej dobrana rasa psa dla Ciebie to {breed_name}!\n")
+    print(translated_description)
+    image = get_dog_image(breed_name)
+
+    if image:
+        print(f"{image}")
+    else:
+        print(f"Niestety nie udało się znaleźć zdjęcia tej rasy.")
+else:
     print("\nBrak wyników po filtracji.")
--- prompts.py
+++ prompts.py
@@ -4,14 +4,15 @@ Prompty dla różnych kategorii
 
 WEIGHT_PROMPT = """
 Użytkownik wprowadził następujące zdanie: "{user_input}".
-Twoim zadaniem jest zinterpretowanie intencji użytkownika i określenie, które kategorie psów chce wybrać:
+Twoim zadaniem jest określenie preferencji użytkownika dotyczących wielkości psa, stosując poniższe zasady:
+Kategorie:
 1 – Małe psy (poniżej 10 kg)
 2 – Średnie psy (od 10 do 25 kg)
 3 – Duże psy (powyżej 25 kg)
 Zasady:
 1. Jeśli użytkownik wyraźnie wskazuje jedną kategorię, zwróć tylko jej numer (np. "1", "2" lub "3").
 2. Jeśli użytkownik chce wybrać więcej niż jedną kategorię, zwróć numery wybranych kategorii oddzielone przecinkami (np. "1,2").
-3. Jeśli użytkownik zaznacza, że nie ma preferencji lub pozostawia decyzję otwartą, zwróć "1,2,3".
+3. Jeśli użytkownik zaznacza, że nie ma preferencji / brak znaczenia lub pozostawia decyzję otwartą, zwróć "1,2,3".
 4. Jeśli zdanie jest niejasne lub nie pasuje do żadnej kategorii, zwróć "0".
 Pamiętaj:
 - Analizuj kontekst wypowiedzi użytkownika.
@@ -22,15 +23,15 @@ GROOMING_PROMPT = """
 Użytkownik wprowadził następujące zdanie: "{user_input}".
 Twoim zadaniem jest zinterpretowanie intencji użytkownika i określenie, jak często użytkownik może zajmować się pielęgnacją swojego zwierzaka, wybierając odpowiednie kategorie. Oto zasady interpretacji:
 Kategorie:
-2 – Okazjonalne czyszczenie sierści - trwa zwykle kilka minut, wykonywane rzadko.
-4 – Czyszczenie sierści raz w tygodniu - trwa około 10-30 minut, wykonywane raz w tygodniu.
-6 – Czyszczenie 2-3 razy w tygodniu - trwa około 10-30 minut, wykonywane 2-3 razy w tygodniu.
-8 – Czyszczenie sierści codziennie - trwa około 10-30 minut dziennie, co daje 70-210 minut w tygodniu.
-10 – Specjalistyczna pielęgnacja przez profesjonalistę - trwa zwykle kilka godzin dziennie, w zależności od potrzeb zwierzęcia.
+0.2 – Okazjonalne czyszczenie sierści - trwa zwykle kilka minut, wykonywane rzadko.
+0.4 – Czyszczenie sierści raz w tygodniu - trwa około 10-30 minut, wykonywane raz w tygodniu.
+0.6 – Czyszczenie 2-3 razy w tygodniu - trwa około 10-30 minut, wykonywane 2-3 razy w tygodniu.
+0.8 – Czyszczenie sierści codziennie - trwa około 10-30 minut dziennie, co daje 70-210 minut w tygodniu.
+1.0 – Specjalistyczna pielęgnacja przez profesjonalistę - trwa zwykle kilka godzin dziennie, w zależności od potrzeb zwierzęcia.
 Zasady:
-1. Jeśli użytkownik wyraźnie wskazuje jedną kategorię, zwróć tylko jej numer (np. "2").
-2. Jeśli użytkownik wskazuje więcej niż jedną kategorię, wybierz najwyższą kategorię (np. "2,4" -> "4").
-3. Jeśli użytkownik zaznacza brak preferencji lub pozostawia decyzję otwartą, zwróć "10".
+1. Jeśli użytkownik wyraźnie wskazuje jedną kategorię, zwróć tylko jej numer (np. "0.2").
+2. Jeśli użytkownik wskazuje więcej niż jedną kategorię, wybierz najwyższą kategorię (np. "0.2,0.4" -> "0.4").
+3. Jeśli użytkownik zaznacza brak preferencji / brak znaczenia lub pozostawia decyzję otwartą, zwróć "1.0".
 4. Jeśli zdanie jest niejasne, niezrozumiałe lub nie pasuje do żadnej kategorii, zwróć "0".
 Pamiętaj:
 - Analizuj kontekst wypowiedzi użytkownika.
@@ -38,20 +39,90 @@ Pamiętaj:
 """
 
 SHEDDING_PROMPT = """
-Użytkownik wprowadził następujące zdanie: “{user_input}”.
-Twoim zadaniem jest zinterpretowanie intencji użytkownika i określenie jego preferencji co do częstotliwości linienia psa, wybierając odpowiednie kategorie. Oto zasady interpretacji:
+Użytkownik wprowadził następujące zdanie: "{user_input}".
+Twoim zadaniem jest określenie preferencji użytkownika dotyczących częstotliwości linienia psa, stosując poniższe zasady:
 Kategorie:
-2 – Rzadkie linienie, użytkownik preferuje psy, które prawie nie linieją.
-4 – Sporadyczne linienie, użytkownik akceptuje psy z niewielkim linieniem.
-6 – Sezonowe linienie, użytkownik akceptuje psy, które linieją w określonych porach roku.
-8 – Regularne linienie, użytkownik akceptuje psy, które linieją przez większość czasu w umiarkowanym stopniu.
-10 – Częste linienie, użytkownik akceptuje psy, które linieją intensywnie przez cały rok.
+0.2 – Rzadkie linienie, użytkownik preferuje psy, które prawie nie linieją.
+0.4 – Sporadyczne linienie, użytkownik akceptuje psy z niewielkim linieniem.
+0.6 – Sezonowe linienie, użytkownik akceptuje psy, które linieją w określonych porach roku.
+0.8 – Regularne linienie, użytkownik akceptuje psy, które linieją przez większość czasu w umiarkowanym stopniu.
+1.0 – Częste linienie, użytkownik akceptuje psy, które linieją intensywnie przez cały rok.
 Zasady:
-1.	Jeśli użytkownik wyraźnie wskazuje jedną kategorię, zwróć tylko jej wartość (np. “2”).
-2.	Jeśli użytkownik wskazuje więcej niż jedną kategorię, wybierz najwyższą kategorię (np. “4,6” -> “6”).
-3.	Jeśli użytkownik zaznacza brak preferencji lub pozostawia decyzję otwartą, zwróć “10”.
-4.	Jeśli zdanie jest niejasne, niezrozumiałe lub nie pasuje do żadnej kategorii, zwróć “0”.
+1. Jeśli użytkownik wyraźnie wskazuje jedną kategorię, zwróć tylko jej wartość (np. "0.2").
+2. Jeśli użytkownik wskazuje więcej niż jedną kategorię, wybierz najwyższą kategorię (np. "0.4,0.6" -> "0.6").
+3. Jeśli użytkownik zaznacza brak preferencji / brak znaczenia lub pozostawia decyzję otwartą, zwróć "1.0".
+4. Jeśli zdanie jest niejasne lub nie pasuje do żadnej kategorii, zwróć "0".
 Pamiętaj:
 - Analizuj kontekst wypowiedzi użytkownika.
 - Nie dodawaj żadnych dodatkowych wyjaśnień ani tekstu.
+"""
+
+ENERGY_PROMPT = """
+Użytkownik wprowadził następujące zdanie: "{user_input}".
+Twoim zadaniem jest określenie preferencji użytkownika dotyczących zapotrzebowania energetycznego psa, stosując poniższe zasady:
+Kategorie:
+0.2 – Couch Potato, użytkownik preferuje psy o bardzo niskim poziomie aktywności.
+0.4 – Calm, użytkownik akceptuje psy o spokojnym temperamencie i niskim zapotrzebowaniu na ruch.
+0.6 – Regular Exercise, użytkownik akceptuje psy wymagające umiarkowanej ilości codziennego ruchu.
+0.8 – Energetic, użytkownik akceptuje psy o wysokim poziomie energii, które potrzebują dużo aktywności.
+1.0 – Needs Lots of Activity, użytkownik akceptuje psy o bardzo wysokim poziomie energii, wymagające intensywnego ruchu i zajęć.
+Zasady:
+1. Jeśli użytkownik wyraźnie wskazuje jedną kategorię, zwróć tylko jej wartość (np. "0.4").
+2. Jeśli użytkownik wskazuje więcej niż jedną kategorię, wybierz najwyższą kategorię (np. "0.6, 0.8" -> "0.8").
+3. Jeśli użytkownik zaznacza brak preferencji / brak znaczenia lub pozostawia decyzję otwartą, zwróć "1.0".
+4. Jeśli zdanie jest niejasne lub nie pasuje do żadnej kategorii, zwróć "0".
+Pamiętaj:
+- Analizuj kontekst wypowiedzi użytkownika.
+- Nie dodawaj żadnych dodatkowych wyjaśnień ani tekstu.
+"""
+
+TRAINABILITY_PROMPT = """
+Użytkownik wprowadził następujące zdanie: "{user_input}".
+Twoim zadaniem jest określenie preferencji użytkownika dotyczących zdolności psa do szkolenia, stosując poniższe zasady:
+Kategorie:
+0.2 – Może być uparty, użytkownik akceptuje psy, które mogą być trudne do szkolenia.
+0.4 – Niezależny, użytkownik akceptuje psy o niezależnym charakterze, które wymagają większej cierpliwości podczas szkolenia.
+0.6 – Chętny do współpracy, użytkownik preferuje psy, które są skłonne do współpracy i umiarkowanie łatwe w szkoleniu.
+0.8 – Łatwy w szkoleniu, użytkownik preferuje psy, które są łatwe do szkolenia i szybko uczą się poleceń.
+1.0 – Chętny do zadowolenia, użytkownik preferuje psy, które są wyjątkowo chętne do współpracy i bardzo łatwe w szkoleniu.
+Zasady:
+1. Jeśli użytkownik wyraźnie wskazuje jedną kategorię, zwróć tylko jej wartość (np. "0.2").
+2. Jeśli użytkownik wskazuje więcej niż jedną kategorię, wybierz najwyższą kategorię (np. "0.4,0.6" -> "0.6").
+3. Jeśli użytkownik zaznacza brak preferencji / brak znaczenia lub pozostawia decyzję otwartą, zwróć "1.0".
+4. Jeśli zdanie jest niejasne lub nie pasuje do żadnej kategorii, zwróć "0".
+Pamiętaj:
+- Analizuj kontekst wypowiedzi użytkownika.
+- Nie dodawaj żadnych dodatkowych wyjaśnień ani tekstu.
+"""
+
+DEMEANOR_PROMPT = """
+Użytkownik wprowadził następujące zdanie: "{user_input}".
+Twoim zadaniem jest określenie preferencji użytkownika dotyczących reakcji psa na obcych i inne zwierzęta, stosując poniższe zasady:
+Kategorie:
+0.2 – Zdystansowany/Ostrożny, użytkownik preferuje psy, które są bardzo powściągliwe wobec obcych i innych zwierząt.
+0.4 – Powściągliwy wobec obcych, użytkownik akceptuje psy, które zachowują umiarkowany dystans wobec obcych.
+0.6 – Czujny/Reagujący, użytkownik akceptuje psy, które są czujne i reagują na obecność obcych lub innych zwierząt.
+0.8 – Przyjazny, użytkownik akceptuje psy, które są otwarte i przyjazne wobec obcych oraz innych zwierząt.
+1.0 – Towarzyski, użytkownik preferuje psy, które są bardzo towarzyskie i uwielbiają kontakt z obcymi oraz innymi zwierzętami.
+Zasady:
+1. Jeśli użytkownik wyraźnie wskazuje jedną kategorię, zwróć tylko jej wartość (np. "0.2").
+2. Jeśli użytkownik wskazuje więcej niż jedną kategorię, wybierz najwyższą kategorię (np. "0.4,0.6" -> "0.6").
+3. Jeśli użytkownik zaznacza brak preferencji / brak znaczenia lub pozostawia decyzję otwartą, zwróć "1.0".
+4. Jeśli zdanie jest niejasne lub nie pasuje do żadnej kategorii, zwróć "0".
+Pamiętaj:
+- Analizuj kontekst wypowiedzi użytkownika.
+- Nie dodawaj żadnych dodatkowych wyjaśnień ani tekstu.
+"""
+
+TEMPERAMENT_PROMPT = """
+Użytkownik wprowadził następujące zdanie: "{user_input}".
+Twoim zadaniem jest określenie najbardziej zbliżonych cech charakteru psa z poniższej listy:
+{prompt_data}
+Jeśli podane przez użytkownika cechy nie pokrywają się z żadnym z temperamentów psów wybierz rasę, która jest najbardziej uniwersalna lub neutralna pod względem cech charakteru. 
+Zwróć tylko jedną rasę psa - bez żadnych dodatkowych słów.
+"""
+
+TRANSLATE_PROMPT = """
+Przetłumacz tekst na język polski:
+"{text}"
 """
